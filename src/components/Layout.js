d95b6385a4b12653cc2ae414ea47411dcb320108
neonboxx <johnbradshaw1991@hotmail.com>
2020-11-20 08:10:27 +0000

    adds og image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -55,7 +55,9 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix("/")}img/og-image.jpg`}
+          content={`${withPrefix(
+            "/"
+          )}img/shf-veterinary-physiotherapy-social-assets-cover-photo-2.jpg`}
         />
       </Helmet>
       <div>{children}</div>
